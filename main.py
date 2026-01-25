e7dfa4c66f04bfacba0557302d6facc04045c988
Aleksander Brk <106615154+BRKME@users.noreply.github.com>
2026-01-26 00:47:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from yandex_disk import YandexDiskClient
 from telegram_publisher import TelegramPublisher
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
         logging.StreamHandler(),
